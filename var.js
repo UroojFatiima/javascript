c329b8960cf1bc7f324e5c434199db84b260cc9c
UroojFatiima <135306978+UroojFatiima@users.noreply.github.com>
2023-06-16 12:06:12 +0500

    This is javascript file

---
+++ var.js
@@ -0,0 +1,47 @@
+// 1. Variables in Js 
+var a = 3;
+var b =4;
+console.log(a+b);
+document.write(a+b);
+
+/*  2.DAta types in Js 
+There are two types of datatypes in Javascript
+
+1. Primitive Data types(6)- Numbers, strings , boolean, undefined , null, symbol(abi symbol ko ignore kro)
+2.References Data types- Objects and arrays 
+
+*/
+
+// Numbers 
+var nm1=23;
+var nm2=33;
+console.log(nm1+nm2);
+
+// String 
+var str="my datatype is string";
+console.log(str);
+
+// Boolean
+var a=true;
+var b = false;
+console.log(a, b);
+
+// Undefined null 
+
+var ghs;
+console.log(ghs);
+
+var b= null;
+console.log(b);
+
+// Objects
+var prices = {
+    soap:23,
+    shampoo:55
+}
+console.log(prices);
+
+// Arrays
+var ar1=[1,2,3,4,"arujj"];
+console.log(ar1);
+
---
+++ variable.js
@@ -0,0 +1,30 @@
+var a=5;
+var b=7;
+console.log("a+b is ",a+b);
+
+//string
+var str1="this is string";
+var str2="this is also a string";
+
+// Objects
+
+var marks ={
+    saba:78,
+    almas: 788,
+    hina:998
+}
+//booleans
+var a=true;
+var c=false;
+
+console.log(a);
+console.log(c);
+
+var und=undefined;
+var nll= null;
+// reference datatypes
+// objects and arrays 
+
+// Arrays
+
+
